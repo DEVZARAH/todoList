97fe78f27e240a6fc9759a633e761c004731500c
DevZarah <mojeedomowunmi3@gmail.com>
2022-08-06 17:39:07 +0100

    first

---
+++ js/index.js
@@ -0,0 +1,75 @@
+let input = document.querySelector("#text");
+let display = document.querySelector(".display");
+let taskList = [];
+
+const addItemBottom = () => {
+  if (input.value == "") {
+    alert("Add to task");
+  } else {
+    taskList.push(input.value);
+  }
+
+  console.log(taskList);
+  input.value = "";
+  const displayText = taskList
+    .map((item) => {
+      return `<div class="item">${item}</div>`;
+    })
+    .join("");
+  display.innerHTML = displayText;
+};
+
+const addItemTop = () => {
+  if (input.value == "") {
+    alert("Add to task");
+  } else {
+    taskList.unshift(input.value);
+  }
+
+  console.log(taskList);
+  input.value = "";
+  const displayText = taskList
+    .map((item) => {
+      return `<div class="item">${item}</div>`;
+    })
+    .join("");
+  display.innerHTML = displayText;
+};
+const removeItemTop = () => {
+  taskList.shift(input.value);
+
+  const displayText = taskList
+    .map((item) => {
+      return `<div class="item">${item}</div>`;
+    })
+    .join("");
+  display.innerHTML = displayText;
+};
+const removeItemBottom = () => {
+  taskList.pop(input.value);
+
+  const displayText = taskList
+    .map((item) => {
+      return `<div class="item">${item}</div>`;
+    })
+    .join("");
+  display.innerHTML = displayText;
+};
+
+const refresh = () => {
+  const displayText = taskList
+    .map((item) => {
+      return `<div class="item">${item}</div>`;
+    })
+    .join("");
+  display.innerHTML = " ";
+
+  alert("You have refreshed this page");
+};
+document.querySelector(".six").addEventListener("click", () =>{
+
+    input.value = ""
+    display.innerHTML = " "
+    taskList =[]
+    alert("All cleared")
+})
